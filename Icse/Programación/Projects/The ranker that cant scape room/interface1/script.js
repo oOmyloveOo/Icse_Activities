13a27a7915d2c1f4f9b1582dadae8995b8170035
oOmyloveOo <ukesan2005@gmail.com>
2024-05-15 13:37:46 +0100

    15/05/2024

---
+++ Icse/Programación/Projects/The ranker that cant scape room/interface1/script.js
@@ -0,0 +1,17 @@
+function showNext(currentContentId, nextContentId) {
+  var currentContent = document.getElementById(currentContentId);
+  var nextContent = document.getElementById(nextContentId);
+  currentContent.style.display = "none";
+  nextContent.style.display = "block";
+}
+
+function showPrev(currentContentId, prevContentId) {
+  var currentContent = document.getElementById(currentContentId);
+  var prevContent = document.getElementById(prevContentId);
+  currentContent.style.display = "none";
+  prevContent.style.display = "block";
+}
+
+function showPlay() {
+  // Aquí puedes agregar la lógica para reproducir algo si es necesario
+}
