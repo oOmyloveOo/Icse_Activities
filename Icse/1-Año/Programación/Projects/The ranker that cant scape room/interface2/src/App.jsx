e3c1a9f47f446f7ba1b144dfe292cd13b38e02b8
oOmyloveOo <ukesan2005@gmail.com>
2024-05-28 10:12:31 +0100

    si

---
+++ Icse/1-Año/Programación/Projects/The ranker that cant scape room/interface2/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import React, { useState } from 'react';
+
+export function App(){
+    return(        
+      <main>
+        <div>
+            <img src="img/ruby.png" className='ruby'/>
+            <img src="img/chest.png" className='chest'/>
+            <img src="img/door_lock.png" className='door-lock'/>
+            <img src="img/key.png" className='key'/>
+            <img src="img/skulls.png" className='skulls'/>
+            <img src="img/sheet.png" className='sheet'/>
+            <img src="img/door.png" className='door'/>
+        </div>
+      </main>)
+}
