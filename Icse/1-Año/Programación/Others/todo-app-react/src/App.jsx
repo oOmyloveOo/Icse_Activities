369bfa6b844b7114ddc7ab944a9b1025aea4ae7e
oOmyloveOo <ukesan2005@gmail.com>
2024-06-12 12:42:52 +0100

    a

--- Icse/1-Año/Programación/Others/todo-app-react/src/App.jsx
+++ Icse/1-Año/Programación/Others/todo-app-react/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { TaskTrashIcon } from 'components'
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
